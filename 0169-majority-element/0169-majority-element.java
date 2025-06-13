1f1165be1cfea7f46132f5383341037ed98c89cb
mr.jashwanthreddy <89520760+JashwanthReddy13189@users.noreply.github.com>
2025-06-13 11:47:47 +0530

    Time: 1 ms (99.84%), Space: 53.6 MB (14.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -11,13 +11,6 @@ class Solution {
             else
                 cnt--;
         }
-        int cnt1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == ele)
-                cnt1++;
-        }
-        if (cnt1 > n / 2)
-            return ele;
-        return -1;
+        return ele;
     }
 }
