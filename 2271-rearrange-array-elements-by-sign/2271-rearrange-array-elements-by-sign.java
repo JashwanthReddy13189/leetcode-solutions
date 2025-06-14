06d70f7c8164e2c1a96d7304c7a8df26c9d13805
mr.jashwanthreddy <89520760+JashwanthReddy13189@users.noreply.github.com>
2025-06-14 12:56:11 +0530

    Time: 3 ms (100%), Space: 77.1 MB (48.54%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,7 +1,22 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        // Optimal Approach
+        // Using Two Indexes pos and neg
+        int pos = 0, neg = 1, n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                ans[pos] = nums[i];
+                pos += 2;
+            } else {
+                ans[neg] = nums[i];
+                neg += 2;
+            }
+        }
+        return ans;
+
         // Brute Force Approach
-        int n = nums.length;
+        /*int n = nums.length;
         int[] ans = new int[n];
         List<Integer> positives = new ArrayList<>();
         List<Integer> negatives = new ArrayList<>();
@@ -15,6 +30,6 @@ class Solution {
             ans[2 * i] = positives.get(i);
             ans[2 * i + 1] = negatives.get(i);
         }
-        return ans;
+        return ans;*/
     }
 }
