156d0e3bac4c4c337605fb5341e9d4451b655109
mr.jashwanthreddy <89520760+JashwanthReddy13189@users.noreply.github.com>
2025-06-17 15:08:50 +0530

    Time: 5 ms (17.69%), Space: 43.1 MB (83.05%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,6 +1,8 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
+        // Brute Force Approach
+        // Using Sets
+        /*Set<Integer> set1 = new HashSet<>();
         Set<Integer> resultSet = new HashSet<>();
         for (int num : nums1) {
             set1.add(num);
@@ -10,6 +12,27 @@ class Solution {
                 resultSet.add(num);
             }
         }
-        return resultSet.stream().mapToInt(Integer::intValue).toArray();
+        return resultSet.stream().mapToInt(Integer::intValue).toArray();*/
+
+        // Optimal approach
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                // Found intersection, add to result if not already added
+                if (result.isEmpty() || result.get(result.size() - 1) != nums1[i]) {
+                    result.add(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
