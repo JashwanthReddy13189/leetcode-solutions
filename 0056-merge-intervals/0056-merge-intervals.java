8991907e4467bf32ae027a7352be84fac42bf6ac
mr.jashwanthreddy <89520760+JashwanthReddy13189@users.noreply.github.com>
2025-08-14 08:17:49 +0530

    Time: 8 ms (85.63%), Space: 47 MB (15.8%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // brute force approach
-        // first check if intervals are only one return intervals
-        if(intervals.length <= 1) return intervals;
+        // optimal approach
         
         // sort the given intervals with given start times
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
@@ -13,23 +11,16 @@ class Solution {
 
         for( int i=0; i<n; i++) {
 
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-             // check if res has already travelled intervals and 
-            // skip if current interval is already covered by the last one
-            if(!result.isEmpty() && end <= result.getLast()[1]) continue;
-
-            // merge overlapping intervals
-            for(int j=i+1; j<n; j++) {
-                if(intervals[j][0] <= end) {
-                    end = Math.max(end, intervals[j][1]);
-                } else {
-                    break;
-                }
+            // check if current interval does not
+            // lie in result list
+            if(result.isEmpty() || intervals[i][0] > result.getLast()[1] ) {
+                result.add(new int[]{intervals[i][0], intervals[i][1]});
+            } 
+            // check if current interval
+            // lies in result list
+            else {
+                result.getLast()[1] = Math.max(intervals[i][1], result.getLast()[1]);
             }
-
-            result.add(new int[]{start, end});
 
         }
 
